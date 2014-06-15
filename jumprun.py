32962413d3b65ac8b8c412936d00cb1c3cd9b642
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-06-15 12:20:02 +0500

    Minor update

--- jumprun.py
+++ jumprun.py
@@ -39,7 +39,7 @@ def main():
     """
     This is the main function run by *entry_point* in setup.py
     """
-    arg = docopt(__doc__, version=0.7)
+    arg = docopt(__doc__, version=0.71)
     #creates a hidden database in users/documents
     db_path = os.path.expanduser("~/")
     db_path = db_path + "/" + ".jumprun"
